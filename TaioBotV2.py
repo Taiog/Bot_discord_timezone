e9879d4265549e86409a1d755e7909e6558b6ea6
Tiago <tiagoargollo@hotmail.com>
2022-09-21 14:54:02 -0300

    msg

--- TaioBotV2.py
+++ TaioBotV2.py
@@ -8,26 +8,11 @@ import os
 load_dotenv()
 
 
-def calculateTime(timezone):
-    hora = datetime.datetime.now()
-    hora_string = hora.strftime("%A - %d %B/%y - %I:%M%p")
-    horaUTC = ":united_nations:" + hora_string
-    print(horaUTC)
-
-    tz_BR = pytz.timezone("America/Recife")
-    datetime_BR = datetime.datetime.now(tz_BR)
-    horaBR = ":flag_br:" + datetime_BR.strftime("%A - %d %B/%y - %I:%M%p")
-    print(horaBR)
-
-    tz_CA = pytz.timezone("America/Regina")
-    datetime_CA = datetime.datetime.now(tz_CA)
-    horaCA = ":flag_ca:" + datetime_CA.strftime("%A - %d %B/%y - %I:%M%p")
-    print(horaCA)
-
-    tz_SE = pytz.timezone("Europe/Amsterdam")
-    datetime_SE = datetime.datetime.now(tz_SE)
-    horaSE = ":flag_se:" + datetime_SE.strftime("%A - %d %B/%y - %I:%M%p")
-    print(horaSE)
+def calculateTime(country, zone):
+    time = pytz.timezone(zone)
+    timenow = datetime.datetime.now(time)
+    print(country + timenow.strftime("%A - %d %B/%y - %I:%M%p"))
+    return country + timenow.strftime("%A - %d %B/%y - %I:%M%p")
 
 
 class MyClient(discord.Client):
@@ -37,7 +22,7 @@ class MyClient(discord.Client):
     async def on_message(self, message):
         print(f"Message from {message.author}: {message.content}")
         if message.content == ".hora":
-            await message.channel.send(calculateTime)
+            await message.channel.send(calculateTime(":flag_br:", "America/Recife"))
 
 
 intents = discord.Intents.default()
@@ -46,3 +31,29 @@ intents.message_content = True
 client = MyClient(intents=intents)
 
 client.run(os.getenv("TOKEN"))
+
+
+# def calculateTime(country = '', zone = ''):
+#    hora = datetime.datetime.now()
+#
+#   tz_BR = pytz.timezone("America/Recife")
+#  datetime_BR = datetime.datetime.now(tz_BR)
+#
+#   tz_CA = pytz.timezone("America/Regina")
+#  datetime_CA = datetime.datetime.now(tz_CA)
+#
+#   tz_SE = pytz.timezone("Europe/Amsterdam")
+#  datetime_SE = datetime.datetime.now(tz_SE)
+#
+#
+#  hora_string = hora.strftime("%A - %d %B/%y - %I:%M%p")
+# horaUTC = ":united_nations:" + hora_string
+# print(horaUTC)
+#
+#   horaBR = ":flag_br:" + datetime_BR.strftime("%A - %d %B/%y - %I:%M%p")
+#  print(horaBR)
+#
+#   horaCA = ":flag_ca:" + datetime_CA.strftime("%A - %d %B/%y - %I:%M%p")
+#  print(horaCA)
+# horaSE = ":flag_se:" + datetime_SE.strftime("%A - %d %B/%y - %I:%M%p")
+# print(horaSE)
