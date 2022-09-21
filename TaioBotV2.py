4da53410a58c97cadbf5e6c2d6026625eb89f3cd
Tiago <tiagoargollo@hotmail.com>
2022-09-21 14:22:22 -0300

    msg

--- TaioBotV2.py
+++ TaioBotV2.py
@@ -7,21 +7,27 @@ import os
 
 load_dotenv()
 
-hora = datetime.datetime.now()
-hora_string = hora.strftime("%A - %d %B/%y - %I:%M%p")
-horaUTC = ":united_nations:" + hora_string
 
-tz_BR = pytz.timezone("America/Recife")
-datetime_BR = datetime.datetime.now(tz_BR)
-horaBR = ":flag_br:" + datetime_BR.strftime("%A - %d %B/%y - %I:%M%p")
+def calculateTime(timezone):
+    hora = datetime.datetime.now()
+    hora_string = hora.strftime("%A - %d %B/%y - %I:%M%p")
+    horaUTC = ":united_nations:" + hora_string
+    print(horaUTC)
 
-tz_CA = pytz.timezone("America/Regina")
-datetime_CA = datetime.datetime.now(tz_CA)
-horaCA = ":flag_ca:" + datetime_CA.strftime("%A - %d %B/%y - %I:%M%p")
+    tz_BR = pytz.timezone("America/Recife")
+    datetime_BR = datetime.datetime.now(tz_BR)
+    horaBR = ":flag_br:" + datetime_BR.strftime("%A - %d %B/%y - %I:%M%p")
+    print(horaBR)
 
-tz_SE = pytz.timezone("Europe/Amsterdam")
-datetime_SE = datetime.datetime.now(tz_SE)
-horaSE = ":flag_se:" + datetime_SE.strftime("%A - %d %B/%y - %I:%M%p")
+    tz_CA = pytz.timezone("America/Regina")
+    datetime_CA = datetime.datetime.now(tz_CA)
+    horaCA = ":flag_ca:" + datetime_CA.strftime("%A - %d %B/%y - %I:%M%p")
+    print(horaCA)
+
+    tz_SE = pytz.timezone("Europe/Amsterdam")
+    datetime_SE = datetime.datetime.now(tz_SE)
+    horaSE = ":flag_se:" + datetime_SE.strftime("%A - %d %B/%y - %I:%M%p")
+    print(horaSE)
 
 
 class MyClient(discord.Client):
@@ -31,10 +37,7 @@ class MyClient(discord.Client):
     async def on_message(self, message):
         print(f"Message from {message.author}: {message.content}")
         if message.content == ".hora":
-            await message.channel.send(horaUTC)
-            await message.channel.send(horaBR)
-            await message.channel.send(horaCA)
-            await message.channel.send(horaSE)
+            await message.channel.send(calculateTime)
 
 
 intents = discord.Intents.default()
